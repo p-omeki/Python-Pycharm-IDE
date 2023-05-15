ec18f1c52cb230a37a8b9769f11d2b7640b22a5a
unknown <partymeki@gmail.com>
2023-05-15 17:38:26 +0300

    Created Demo3 for List

---
+++ demo3.py
@@ -0,0 +1,15 @@
+#Creating List in Python
+'''
+List in python are data structures that allow one to store data on to it. Similarly, lists are like arrays in other programming languages.
+And what are arrays? A collection of elements of the same kind placed together in a contiguous(Sharing a border) memory location and can
+be individually referenced by using an index to a unique identifier.
+'''
+#Creating a list of my pals in University
+Names = ["Patrick", "Bahati", "Klein", "Nigel", "Naftal", "Moyale"] #First, we've declared our values in square brackets.
+
+print(Names) #This will print all the names in the List.
+
+'''However, list also have positions in them and they're identified using an index [0.....] which starts from 0. So, if we want
+to access "Moyale" individally, we'll specify index[5]
+'''
+print(Names[5])
