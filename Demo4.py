adca020a749a6682c5cb6db9a45f4e7caa0d31df
unknown <partymeki@gmail.com>
2023-05-16 12:06:26 +0300

    Created Demo4 for Range

---
+++ Demo4.py
@@ -0,0 +1,13 @@
+#Range in Python
+
+Numbers = list(range(1, 20, 3))
+
+print(Numbers)
+'''So, in the above code, we finding the range in python and Range returns a sequence of numbers and it starts by 0 by 
+default. In our case, the we've specified the range to start from 1, 20 stands for the number of sequence and 3 stands 
+for the increament for our range
+
+Well, you might probably wonder why we using the list() instead of list[] to enclose range(), it's because we need to 
+convert a range objects into a list. If we run the range direct without enclosing it inside the list, we'll just 
+have a normal printout output.
+'''
