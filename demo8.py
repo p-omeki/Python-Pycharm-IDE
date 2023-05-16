3d2202fe3c99318a9bc3f563ea61d7c313978102
unknown <partymeki@gmail.com>
2023-05-16 15:24:46 +0300

    Created Demo9 for Passing Arguments

--- demo8.py
+++ demo8.py
@@ -2,4 +2,4 @@
 counter = 0
 while counter <=10: #As long as the loop is still below 10, the loop continues execution
      print(counter)
-     counter += 1 #Defines the increament and decreament
+     counter += 2 #Defines the increament and decreament
