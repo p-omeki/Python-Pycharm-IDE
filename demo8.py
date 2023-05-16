facc6f084e269dcc699bb4d76d1b46a11d712347
unknown <partymeki@gmail.com>
2023-05-16 15:03:53 +0300

    Created Demo7 for While Loop

---
+++ demo8.py
@@ -0,0 +1,5 @@
+#While Loop in Python
+counter = 0
+while counter <=10: #As long as the loop is still below 10, the loop continues execution
+     print(counter)
+     counter += 1 #Defines the increament and decreament
