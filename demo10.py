9d548253f6d1268418c2a4b2f284796d30585909
unknown <partymeki@gmail.com>
2023-05-17 11:32:22 +0300

    Created Demo10 for Functions Returning Value

--- demo10.py
+++ demo10.py
@@ -1,1 +1,9 @@
 #Making functions return value in Python
+
+def add(a, b): #We first define our function by specifying the arguments.
+    c = a+b   #We define a variable the we'll store our arguments.
+    return c  #We return the variable that contains our arguments.
+
+result = add(10, 20) #Now, we want our variable c to be returned as a result and so we define variable result which we'll store our c.
+
+print(result) #This will print out the additions of the arguments passed and so the function would have returned a value.
