ee90301ae4b47877fa14698f10f3f1d88ecf4902
unknown <partymeki@gmail.com>
2023-05-21 19:32:10 +0300

    Created Demo14 for File Handling

--- demo14.py
+++ demo14.py
@@ -2,9 +2,9 @@
 '''
 File handling deals with the writing and reading of files one is using in his code or the databases.
 In this part, we'll create a .txt file which we'll write, "w" and read, "r" to it.
-We've created a demo14.txt.py file...
+We've created a demo14.txt file...
 '''
-file = open("demo14.txt.py", "w")
+file = open("demo14.txt", "w")
 '''On the above line of code, we've initialized the file to be opened using the function open() which we have 
 passed parameters inside. The first parameters we have is the file name that we need to pass the file name we'll access
 and the second parameter is the mode we want to use to access the file, either write or read mode.
