a5c27d0f52667fc4f92f6d4a462192f97ba0c807
unknown <partymeki@gmail.com>
2023-05-22 16:33:21 +0300

    Created Demo15 for Dictionaries

---
+++ demo15.py
@@ -0,0 +1,12 @@
+#Dictionaries in Python
+'''
+In a simple definition of Dictionary, we'll say it's a book that you look up to it for meaning of words.
+Now, in python, dictionaries are types of data structures that map random keys to their respective values.
+Dictionaries are similar to list but there are several  differences among them that stands out. This are:
+  1. Dictionaries are enclosed in curly braces while list are in square brackets.
+  2. In Dict., it allows storage of more than one data to it while in list you can only pass a single line of data.
+'''
+# Creating a dictionary of people and their respective age.
+people = {"Patrick": 23, "John Doe": 50} #We've declared two data Patrick and John Doe with their respective age.
+
+print(people["Patrick"]) #This will print the age of John Doe, 50.
