b1ead94be179329ada0581b6aa19a438d30422e0
unknown <partymeki@gmail.com>
2023-05-23 21:55:33 +0300

    Created Demo19 for Numeric functions

---
+++ demo19.py
@@ -0,0 +1,16 @@
+#Numeric functions in python
+'''
+I'll focus on 3 main numeric functions. That is, minimum, maximum and absolute functions.
+Check the example below...
+'''
+#Min function ~ This will print the minimum number
+numbers = 167, 28, 356, 47, 56, 68
+print(min(numbers))
+
+#Max function ~ This will print the maximum number
+numbers = 167, 28, 356, 47, 56, 68
+print(max(numbers))
+
+#Absolute function ~ This will give the absolute number without - or + sign
+numbers = -58 #We can only assign one absolute number at a time
+print(abs(numbers))
