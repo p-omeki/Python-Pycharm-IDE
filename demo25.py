fdccb893cf717df7dadf011eaf976f737aa9b6d6
unknown <partymeki@gmail.com>
2023-05-29 20:08:36 +0300

    Created Demo25 for OOP

--- demo25.py
+++ demo25.py
@@ -2,7 +2,7 @@
 '''
 OOP as the name suggest is about objects, the creation of objects and usage of objects as instance of a class. And what
 is a class? It's a blueprint for creating objects. So, for an object to exist, there must be a class.
-In OOP, there are 4 major aspects we'll look up to in depth, these are: A class, an attribute, a method and lastly
+In OOP, there are 4 major aspects we'll look up to in depth, these are: A class, an attribute/properties, a method and lastly
 object. Clearly, when it comes to object-oriented programing, we define our code in that order...
 Class->Attribute->Method->Object. The attribute is now the variable stored in an instance or class or in a layman's
 language, it describes the class. The method is a function stored in an instance or class, and it's used to define what
