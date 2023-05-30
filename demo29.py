5cf59b24a737278eb0781cdd5705453810a42277
unknown <partymeki@gmail.com>
2023-05-30 19:07:10 +0300

    Created Demo29 for Itertools

--- demo29.py
+++ demo29.py
@@ -1,1 +1,13 @@
 #Itertools in python
+'''
+Itertools are modules used as tools in functional programming. Since they're modules, for us to use them we simply have
+to import them to our code. Itertools have different function used with it, but on this article we are going to focus
+on three functions that are mostly used. This includes:
+       1.The count ~ The function iterates a number to infinite once the start is specified.
+       2.The accumulate ~ The function adds the corresponding
+       3.The takewhile ~ The function takes an element from a list while a certain condition is TRUE.
+
+'''
+#Count function
+from itertools import count
+
