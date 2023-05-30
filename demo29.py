96e7e840f8993f484ff727aa3d53ca65b63f76d3
unknown <partymeki@gmail.com>
2023-05-30 19:28:31 +0300

    Created Demo29 for Itertools

--- demo29.py
+++ demo29.py
@@ -9,5 +9,25 @@ on three functions that are mostly used. This includes:
 
 '''
 #Count function
-from itertools import count
+'''from itertools import count    #This line states, from module itertools, import function count...
+
+for i in count(3):
+    if i > 20:
+        break
+    print(i) #This will print to infinite, so we set a condition to break when it reaches a certain limit. Our Max is 20
+    '''
+#Accumulate and Takewhile
+from itertools import accumulate, takewhile
+
+numbers = list(accumulate(range(8))) #Creating list numbers
+
+numbers = list(takewhile(lambda x: x <= 10, numbers)) #This will print numbers less than 10 from the list...
+
+print(numbers)
+
+   ##Output: [0, 1, 3, 6, 10, 15, 21, 28]
+'''Notice, how the output looks...it adds the previous to the next and finds the sum, again it has generated a list 
+of 8 elements just as we have specified in the range(8).
+'''
+
 
