caaee136706bc093e2a8715d1b2f5a5aeb157d98
unknown <partymeki@gmail.com>
2023-06-01 21:44:46 +0300

    Demo30

--- demo30.py
+++ demo30.py
@@ -4,7 +4,8 @@ Operator overloading is a method that allows us to perform an operation on a cod
 overloading an operator, +/- . Now, a normal add or sub operation we'll declare the operation and run the code, here
 the +/- result is given based on the particular function definition we've defined and so, we decide what the operator
 will do, not just add automatically.
-In summary, it allows one operator to be used with different meaning.
+In summary, it allows a programmer to define the behaviour of an operator when applied to a user-defined objects or
+classes.
 Check the code below..
 '''
 #Performing an operator overloading to calculate two coordinates
