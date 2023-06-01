a6a78b6515491f30059e12594c73e894ba852dc3
unknown <partymeki@gmail.com>
2023-06-01 21:27:40 +0300

    Demo30

--- demo30.py
+++ demo30.py
@@ -1,1 +1,8 @@
 #Operator Overloading In Python
+'''
+Operator overloading is a method that allows us to perform an operation on a code, either addition or subtraction by
+overloading an operator, +/- . Now, a normal add or sub operation we'll declare the operation and run the code, here
+the +/- result is given based on the particular function definition we've defined and so, we decide what the operator
+will do, not just add automatically.
+Check the code below..
+'''
