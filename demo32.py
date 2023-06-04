12247e618a63bfc277c7c69813f4196081d07be3
unknown <partymeki@gmail.com>
2023-06-04 12:53:27 +0300

    Demo32

--- demo32.py
+++ demo32.py
@@ -1,1 +1,7 @@
 #Regular Expression in Python
+'''
+Regular expression is a module that allows us to modify strings in python. Regular expression is used perform to major
+tasks in python, this includes;
+ 1. Checking if a string matches a particular pattern.
+ 2. Perform substitution in strings.
+'''
