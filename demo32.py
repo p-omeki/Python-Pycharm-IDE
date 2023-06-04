3ff2ee583e75dafa5f1cb804a5fd3228af11a826
unknown <partymeki@gmail.com>
2023-06-04 13:43:00 +0300

    Created Demo32 for Regular Expression in Python

--- demo32.py
+++ demo32.py
@@ -5,12 +5,31 @@ tasks in python, this includes;
  1. Checking if a string matches a particular pattern.
  2. Perform substitution in strings.
 '''
+# Match function in Regular Expression
 import re
 Rpattern = r"mail"  #The 'r' prefix is used to create raw strings for our pattern, in this case, 'mail'
-if re.match(Rpattern, "mailpartymeki@gmail.com"):
+if re.match(Rpattern, "mailpartymeki@gmail.commailpatohmail"):
    print("Match Found")
 else:
-   print("No match found")
+   print("No Match Found")
+'''Notice, that if we put any name first aprt from mail, we find a "No match found", that's what the match function 
+actually does, and so, that's an disadvantage, so how do we fix this?. We're going to use the search function, this will
+look for the mail pattern anywhere around your string.Lastly, we have the findall function that checks the number of 
+times the 'mail' is present in the the string.
+Check the example below...
+'''
+#We using the same import re and Rpattern, so we'll continue from if...
+if re.search(Rpattern, "mekimailpartymeki@gmail.commailpatohmail"):
+   print("Match Found")
+else:                         #This will print a Match Found coz the 'mail', is not necessarily at the start but somewhere in the string.
+   print("No Match Found")
+#Findall function now,
+'''
+if re.findall(Rpattern, "mekimailpartymeki@gmail.commailpatohmail"):
+   print("Match Found")
+else:
+   print("No Match Found")
+   '''
+print(re.findall(Rpattern, "mekimailpartymeki@gmail.commailpatohmail")) #This will print the exact word, 'mail' 4 times as we've used in our string.
 
-#re.match//re.search//re.sub
-  #re.match
+
