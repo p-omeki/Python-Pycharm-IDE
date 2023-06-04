e5f76ac9d0dbd58e96d20cf49be178acb6d3bffa
unknown <partymeki@gmail.com>
2023-06-04 13:00:15 +0300

    Created Demo32 for Regular Expression

--- demo32.py
+++ demo32.py
@@ -5,3 +5,9 @@ tasks in python, this includes;
  1. Checking if a string matches a particular pattern.
  2. Perform substitution in strings.
 '''
+import re
+Rpattern = r"mail"  #The 'r' stands for raw and it initialize the string we want to check the pattern for, in this case, 'mail'
+if re.match(Rpattern, "partymeki@gmail.com"):
+   print("Match Found")
+else:
+   print("No match found")
