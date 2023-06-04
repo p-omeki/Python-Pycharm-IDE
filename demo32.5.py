2bc648e86b3fea30bc106330b422e9bf202385dd
unknown <partymeki@gmail.com>
2023-06-04 15:29:45 +0300

    Created Demo32.5 for

---
+++ demo32.5.py
@@ -0,0 +1,9 @@
+#Meta (*) character
+'''
+This metacharacter matches zero or more than one occurrence of the preceding character or group.
+The group is denoted by ().
+Check the example below...
+'''
+import re
+
+Rpattern = r"mail(mine)"
