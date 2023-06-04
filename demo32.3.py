fd02d891197ca3164ec6d378792e4d5882555eb9
unknown <partymeki@gmail.com>
2023-06-04 14:39:52 +0300

    Created Demo32.3 for Caret & Dollar Metacharacter

--- demo32.3.py
+++ demo32.3.py
@@ -3,7 +3,7 @@
 The metacharacters are used to specify the starting point and ending point of a string respectively. That is, ^ & $.
 They're majorly used in web frameworks such as Django to specify the URl pattern of a website one is creating.
 '''
-import  re
+import re
 Rpattern = r"^Gr.y$" #We've specified our string to start with G and end with y, so any change we'll result to negative response.
 if (re.match(Rpattern, "Jray")): #If we try to change the starting point G or Y we get -ve feed, in our case replace G with J
     print("Match Found")
