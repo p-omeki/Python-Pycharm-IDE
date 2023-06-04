75d63cb65d35c2b8602297e3939b83bdd626ddc2
unknown <partymeki@gmail.com>
2023-06-04 14:22:21 +0300

    Created Demo32.2 for Dot Metacharacter

--- demo32.2.py
+++ demo32.2.py
@@ -1,8 +1,10 @@
 #The Dot Metacharacter
 '''
 The Dot Metacharacter simply allows us to perform powerful regular expression. We use the '.' just like the name
-suggests.
+suggests. There are two more significant metacharacter in Python that we use, this includes: caret(^) and dollar(&).
+In the example below, we'll discuss the dot.
 '''
+#The Dot (.)
 import re
 Rpattern = r"Gr.y" #What the '.' will do, it will allow any character to be put in the '.' position
 if (re.match(Rpattern, "Grxy")): #Any letter we put in between Gr and y is acceptable, i.e a,e,x,b,z,k,d,p,t,b, anything.
