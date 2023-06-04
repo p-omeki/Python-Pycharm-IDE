5378dd47f5db244f34f700f0e4fa7799cf8dd90e
unknown <partymeki@gmail.com>
2023-06-04 17:34:13 +0300

    Created Demo32.5 for Meta Character and Group

--- demo32.5.py
+++ demo32.5.py
@@ -11,3 +11,9 @@ if re.match(Rpattern, "mail(mine)partymeki@gmail.commailmine"):
     print("Match Found")
 else:
     print("Not Found")
+'''In line 10 of our code, if we remove the 'mail' we'll have a -ve response coz we would have removed the initialized 
+string 'mail' which is the main string. The string enclosed with bracket/ also a (group), '(mine)' now is the primary
+string and we will get a +ve response if we run or not run the string without it...
+In summary, the group indicates that we can have the string, zero, once or more than one number of times when we run 
+our String.
+'''
